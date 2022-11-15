d37d5fe676f3463ab6cdcd6bab760931e0c030aa
Sebastian Navarro <sebastian.nav@live.cl>
2022-11-15 17:16:03 -0300

    proyecto frontend con nextjs junji-maestro

---
+++ pages/establecimiento/index.tsx
@@ -0,0 +1,24 @@
+
+
+
+export default function Home() {
+    return (
+        <div className="wrapper">
+            <div className="container">
+                <div id="welcome">
+                    <h1>
+                        <span>Estamos en, </span>
+                        Establecimientos
+                    
+                    </h1>
+                </div>
+
+                <div id="commands" className="rounded shadow">
+                    
+                    <input placeholder="Buscar Establecimientos"  />
+                </div>
+
+
+            </div>
+        </div>
+    )}
